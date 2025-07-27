7d22798b5954ac38143eeb2bb42e1e112583d16d
Yujin Jeong <76045552+YujinJeong1006@users.noreply.github.com>
2025-07-27 16:44:01 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.21 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        
+        String[] arr = phone_number.split("");
+        
+        for(int i = 0; i < phone_number.length() - 4; i++){
+            arr[i] = "*";
+        }
+        
+        String answer = String.join("",arr);
+        
+        return answer;
+    }
+}
