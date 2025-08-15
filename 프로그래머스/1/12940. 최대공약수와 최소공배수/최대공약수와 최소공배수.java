e257d61bc5b691e244a0c28f95887a272eda7213
Yujin Jeong <76045552+YujinJeong1006@users.noreply.github.com>
2025-08-15 15:56:54 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int min = Math.min(n, m);
+        
+        int gcd = 0; 
+        for(int i = 1;i <= min; i++){
+            if(n % i == 0 && m % i == 0){
+                gcd = i;
+            }
+        }
+        int lcm = (n * m) / gcd;
+
+        return new int[]{gcd,lcm};
+    }
+}
