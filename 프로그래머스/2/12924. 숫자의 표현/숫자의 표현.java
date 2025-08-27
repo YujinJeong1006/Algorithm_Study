ce04bb9f36f1e2334c46af907e49305d07e2dbd7
Yujin Jeong <76045552+YujinJeong1006@users.noreply.github.com>
2025-08-27 17:15:14 +0900

    [level 2] Title: 숫자의 표현, Time: 1.99 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        int count = 1; // n 자신 
+        
+        for(int i = 1; i < n; i++){
+            sum = 0;
+            for(int j = i; j < n; j++){
+                sum += j;
+                
+                if(sum == n){
+                    count++;
+                    break;
+                } else if(sum > n){
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
